a5327e4d32b51f0c2d1004c212dace8ff45c6387
IWaddell01 <i.83.waddell@gmail.com>
2020-09-04 13:40:32 -0500

    Installed react redux and setup store

--- src/App.js
+++ src/App.js
@@ -1,121 +1,150 @@
 import React, { Component } from "react";
 import todosList from "./todos.json";
 import { Route, NavLink } from "react-router-dom";
-import TodoList from "./components/TodoList/TodoList"
-import Completed from "./components/completed/completed"
-import Active from "./components/active/active"
-
-
+import TodoList from "./components/TodoList/TodoList";
+import Completed from "./components/completed/completed";
+import Active from "./components/active/active";
+import { v4 as uuid } from "uuid";
 
 class App extends Component {
+	state = {
+		todos: todosList,
+		formData: {
+			todo1: "",
+		},
+	};
 
+	handleAddToDo = (e) => {
+		e.preventDefault();
+		const newId = uuid();
+		this.setState({
+			todos: [
+				...this.state.todos,
+				{
+					userId: 1,
+					id: newId,
+					title: this.state.formData.todo1,
+					completed: false,
+				},
+			],
+			formData: { todo1: "" },
+		});
+	};
 
-  state = {
-    todos: todosList,
-    formData: {
-      todo1: '',
-  }
-  };
+	handleChange = (event) => {
+		const formData = { ...this.state.formData };
+		formData[event.target.name] = event.target.value;
 
+		this.setState({ formData });
+	};
 
+	handleCheck = (TodoId) => {
+		const check = this.state.todos.map((item) => {
+			if (item.id === TodoId) {
+				return { ...item, completed: !item.completed };
+			}
+			return { ...item };
+		});
 
+		this.setState({ todos: check });
+	};
 
-  test = (e) => {
- e.preventDefault()
-    this.setState({
-      todos: [...this.state.todos, {
-        "userId": 1,
-        "id": (this.state.todos.length + 1),
-        "title": this.state.formData.todo1,
-        "completed": false
-    }], formData : {todo1: ""}})
+	handleDelete = (TodoId) => {
+		const newTodos = this.state.todos.filter((todo) => todo.id !== TodoId);
+		this.setState({ todos: newTodos });
+	};
 
-  }
+	handleClear = (TodoId) => {
+		const deleteCompleted = this.state.todos.filter(
+			(todo) => todo.completed !== true
+		);
 
-  handleChange = (event) => {
-    const formData = {...this.state.formData};
-    formData[event.target.name] = event.target.value;
+		this.setState({ todos: deleteCompleted });
+	};
 
-    this.setState({ formData })
+	render() {
+		return (
+			<section className="todoapp">
+				<header className="header">
+					<h1>todos</h1>
+					<form onSubmit={this.handleAddToDo}>
+						<input
+							type="text"
+							name="todo1"
+							className="new-todo"
+							placeholder="What needs to be done?"
+							value={this.state.formData.todo1}
+							onChange={this.handleChange}
+							autoFocus
+						/>
+					</form>
+				</header>
+				<Route
+					exact
+					path="/"
+					render={(props) => (
+						<TodoList
+							{...props}
+							todos={this.state.todos}
+							handleCheck={this.handleCheck}
+							handleDelete={this.handleDelete}
+							handleClear={this.handleClear}
+						/>
+					)}
+				/>
+				<Route
+					path="/completed"
+					render={(props) => (
+						<Completed
+							{...props}
+							todos={this.state.todos}
+							handleCheck={this.handleCheck}
+							handleDelete={this.handleDelete}
+							handleClear={this.handleClear}
+						/>
+					)}
+				/>
+				<Route
+					path="/active"
+					render={(props) => (
+						<Active
+							{...props}
+							todos={this.state.todos}
+							handleCheck={this.handleCheck}
+							handleDelete={this.handleDelete}
+							handleClear={this.handleClear}
+						/>
+					)}
+				/>
+				<footer className="footer">
+					{/* <!-- This should be `0 items left` by default --> */}
+					<span className="todo-count">
+						<strong>0</strong> item(s) left
+					</span>
+					<ul className="filters">
+						<li>
+							<NavLink exact to="/" activeClassName="selected">
+								All
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/active" activeClassName="selected">
+								Active
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/completed" activeClassName="selected">
+								Completed
+							</NavLink>
+						</li>
+					</ul>
+					<button className="clear-completed" onClick={this.handleClear}>
+						Clear completed
+					</button>
+				</footer>
+			</section>
+		);
+	}
 }
-
-
-  handleCheck = (TodoId) => {
-    const check = this.state.todos.map(
-      (item) => {
-        if(item.id === TodoId) {
-          return {...item, completed: !item.completed}
-        }
-        return {...item}
-      }
-    )
-  
-    this.setState({todos: check})
-  }
-
-  handleDelete = TodoId => {
-    const newTodos = this.state.todos.filter(
-      todo => todo.id !== TodoId
-    )
-    this.setState({ todos: newTodos })
-
-    
-  }
-
-  handleClear = TodoId => {
-    const deleteCompleted = this.state.todos.filter(
-      todo => todo.completed !== true
-    )
-
-    this.setState({ todos: deleteCompleted})
-   }
-
-
-
-
-  render() {
-    return (
-      <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <form onSubmit={this.test}>
-            <input
-                type="text" 
-                name="todo1" 
-                className="new-todo" 
-                placeholder="What needs to be done?"
-                value={this.state.formData.todo1} 
-                onChange={this.handleChange} 
-                
-                autoFocus />
-          </form>
-          
-        </header>
-        <Route exact path="/" render={ (props) => <TodoList {...props} todos={this.state.todos} handleCheck={this.handleCheck} handleDelete={this.handleDelete} handleClear={this.handleClear}/>} />
-    <Route path="/completed" render={ (props) => <Completed {...props} todos={this.state.todos} handleCheck={this.handleCheck} handleDelete={this.handleDelete} handleClear={this.handleClear}/>} />
-        <Route path="/active" render={ (props) => <Active {...props} todos={this.state.todos} handleCheck={this.handleCheck} handleDelete={this.handleDelete} handleClear={this.handleClear}/>}  />
-        <footer className="footer">
-            {/* <!-- This should be `0 items left` by default --> */}
-          <span className="todo-count">
-            <strong>0</strong> item(s) left
-          </span>
-          <ul className="filters">
-            <li>
-              <NavLink exact to="/" activeClassName="selected">All</NavLink>
-            </li>
-            <li>
-              <NavLink to="/active" activeClassName="selected">Active</NavLink>
-            </li>
-            <li>
-              <NavLink to="/completed" activeClassName="selected">Completed</NavLink>
-            </li>
-          </ul>
-          <button className="clear-completed" onClick={this.handleClear}>Clear completed</button>
-        </footer>
-      </section>
-    )
-  }
-}
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
